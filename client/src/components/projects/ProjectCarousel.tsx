7701959eab8238689d2c7f3b20135e3330bbcc0e
Epfryer <Epf2002hi@gmail.com>
2025-01-24 19:49:27 -0500

    modified:   client/src/components/projects/ProjectCarousel.tsx

--- client/src/components/projects/ProjectCarousel.tsx
+++ client/src/components/projects/ProjectCarousel.tsx
@@ -14,32 +14,24 @@ export function ProjectCarousel({ images, onSlideChange }: ProjectCarouselProps)
   if (!images?.length) return null;
 
   return (
-    <div className="w-full h-full">
-      <Swiper
-        modules={[Navigation, Scrollbar, A11y]}
-        navigation
-        slidesPerView={1}
-        scrollbar={{ draggable: true, hide: false }}
-        spaceBetween={0}
-        className="w-full h-full"
-        onSlideChange={(swiper: SwiperType) => onSlideChange?.(swiper.activeIndex)}
-        style={{ width: '100%' }}  // Force container width
-      >
-        {images.map((image, index) => (
-          <SwiperSlide 
-            key={index} 
-            style={{ width: '100%', maxWidth: '100%' }}  // Constrain slide width
-          >
-            <div className="w-full h-full aspect-[17/11]">
-              <img
-                src={image}
-                alt={`Slide ${index + 1}`}
-                className="w-full h-full object-cover"
-              />
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </div>
+    <Swiper
+      modules={[Navigation, Scrollbar, A11y]}
+      navigation
+      slidesPerView={1}
+      scrollbar={{ draggable: true, hide: false }}
+      spaceBetween={0}
+      className="!w-full !h-full"
+      onSlideChange={(swiper: SwiperType) => onSlideChange?.(swiper.activeIndex)}
+    >
+      {images.map((image, index) => (
+        <SwiperSlide key={index}>
+          <img
+            src={image}
+            alt={`Slide ${index + 1}`}
+            className="w-full h-full object-cover"
+          />
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 }
